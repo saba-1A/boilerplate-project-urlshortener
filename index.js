a1d9ebb921ce256bee0e24917afc3098a1417529
saba-1A <sabafathima789nazar@gmail.com>
2025-06-20 23:20:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser')
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,3 +29,38 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String
+})
+
+let URLModel = mongoose.model('URL', urlSchema)
+
+app.post('/api/shorturl/', (req, res) => {
+  console.log(req.body.url)
+  if (req.body.url.slice(0,4) === 'http') {
+    const randId = Math.floor(Math.random() * 99999999999999)
+    const reqUrl = new URLModel({
+        original_url: req.body.url, 
+        short_url: randId
+    })
+    reqUrl.save((err, data) => {
+      if (err) return console.error(err);
+    });
+    
+    res.send({original_url: req.body.url, short_url: randId})
+  } else {
+    res.send({ error: 'invalid url' })
+  }
+})
+
+app.get('/api/shorturl/:urlid?', (req, res) => {
+  console.log(req.params.urlid)
+    URLModel.findOne({short_url: req.params.urlid}, (err, data) => {
+      if (err) return console.error(err)
+      res.writeHead(301, {
+        Location: data.original_url
+      }).end()
+    })
+})
